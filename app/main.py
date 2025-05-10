479ed1ddfc1fa90f40301722b41164a4ffe32a17
Marcos Parra <marcos345dav@gmail.com>
2025-05-10 13:39:55 -0500

    Feat(frontend): Welcome screen

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return "Hola mundo"
+app.mount("/public", StaticFiles(directory="public"), name="public")
+
+@app.get("/", response_class=HTMLResponse)
+def read_html():
+    html_path = Path("public/index.html")
+    return HTMLResponse(content=html_path.read_text(), status_code=200)
